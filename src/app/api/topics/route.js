998e6c21685d54630068026d936a8f7bc7ef8578
saikingalgulwad <saiprasadalgulwad@gmail.com>
2024-09-25 04:28:52 -0700

    it

--- src/app/api/topics/route.js
+++ src/app/api/topics/route.js
@@ -17,7 +17,7 @@ export async function POST(req) {
   return NextResponse.json({Message:"create data"},{status:201});
 
 }
-export async function POST(req) {
+export async function DELETE(req) {
   const id = req.nextUrl.searchParams.get("id");
   await connectMongoDB();
   await Topic.findByIdAndDelete(id);
